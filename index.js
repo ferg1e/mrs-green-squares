5138e3a24beb2a0a3ebf61a80b074f3c813a0765
Ry Ferguson <rytf12@gmail.com>
2016-06-30 13:13:12 -0700

    count number of commits for each day from a single repo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var git = require('nodegit');
 var fs = require('fs');
 
+//
+var commitCounts = {};+
+
 git.Repository.open('C:\\wamp\\www\\pajamasql')
     .then(function(repo) {
         return repo.getMasterCommit();
@@ -11,10 +14,29 @@ git.Repository.open('C:\\wamp\\www\\pajamasql')
 
         history.on('commit', function(commit) {
             ++count;
-            console.log(count);
+            var d = new Date(commit.date());
+
+            //get YYYY-MM-DD
+            var iy = d.getUTCFullYear() + '-' +
+                ('0' + (d.getUTCMonth() + 1)).slice(-2) + '-'  +
+                ('0' + d.getUTCDate()).slice(-2);
+
+            if(commitCounts[iy]) {
+                commitCounts[iy]++;
+            }
+            else {
+                commitCounts[iy] = 1;
+            }
+
+            //console.log(commit.date());
+            //console.log(d.getDay());
+            //console.log(d.toUTCString());
+            //console.log(iy);
+            //console.log();
         });
 
         history.on('end', function(commits) {
+            console.log(commitCounts);
             fs.writeFile(
                 'index.html',
                 `<!doctype html>
