5a42bc76401a29202140d4b8472acf97b87e87cb
Ry Ferguson <rytf12@gmail.com>
2024-09-08 15:26:33 +0100

    create separate functions for getData() and renderData()

--- index.js
+++ index.js
@@ -7,19 +7,9 @@ var monthLabels = [
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-//
-var commitCounts = {};
-var firstDay;
-var lastDay;
-var repoI = -1;
-var count = 0;
-var max = 0;
-var weeksDays;
-var weeksHtml;
-var allColors = [];
-
-//
-openNextRepo();
+getData(config).then(data => {
+    renderData(data)
+})
 
 //
 function getYyyyMmDd(theDate) {
@@ -29,30 +19,38 @@ function getYyyyMmDd(theDate) {
 }
 
 //
-async function openNextRepo() {
-    ++repoI;
+async function getData(configData) {
 
-    if(repoI < config.repos.length) {
-        var repoPath = config.repos[repoI].dir
-            ? config.repos[repoI].dir
-            : config.repos[repoI];
+    //
+    const commitCounts = {}
+    const allColors = []
+    let max = 0
+    let firstDay, lastDay
 
-        var colors = config.repos[repoI].colors
-            ? config.repos[repoI].colors
-            : ['eeeeee', 'D6E685', '8CC665', '44A340', '1E6823'];
-
-        colors = colors.join();
+    //
+    for(let i = 0; i < configData.repos.length; ++i) {
 
         //
-        var colorI = allColors.indexOf(colors);
+        const r = configData.repos[i]
+
+        const repoPath = r.dir
+            ? r.dir
+            : r
+
+        let colors = r.colors
+            ? r.colors
+            : ['eeeeee', 'D6E685', '8CC665', '44A340', '1E6823']
+
+        colors = colors.join()
+
+        let colorI = allColors.indexOf(colors)
 
         if(colorI == -1) {
-            allColors.push(colors);
-            colorI = allColors.indexOf(colors);
+            allColors.push(colors)
+            colorI = allColors.indexOf(colors)
         }
 
-        //
-        var colorIndexStr = colorI.toString();
+        const colorIndexStr = colorI.toString()
 
         //
         const repo = simpleGit({
@@ -63,264 +61,272 @@ async function openNextRepo() {
         const commits = await repo.log()
 
         commits.all.forEach(e => {
-            var authorEmail = e.author_email;
-            var isCountCommit = !config.authors ||
-                config.authors.indexOf(authorEmail) != -1;
+            const authorEmail = e.author_email
+            const isCountCommit = !configData.authors ||
+                configData.authors.indexOf(authorEmail) != -1
 
             if(isCountCommit) {
-                ++count;
-                var d = new Date(e.date);
-                var iy = getYyyyMmDd(d);
+                const d = new Date(e.date)
+                const iy = getYyyyMmDd(d)
 
                 if(!commitCounts[iy]) {
-                    commitCounts[iy] = {};
-                    commitCounts[iy]['total'] = 1;
+                    commitCounts[iy] = {}
+                    commitCounts[iy]['total'] = 1
                 }
                 else {
-                    ++commitCounts[iy]['total'];
+                    ++commitCounts[iy]['total']
                 }
 
                 if(!commitCounts[iy][colorIndexStr]) {
-                    commitCounts[iy][colorIndexStr] = 1;
+                    commitCounts[iy][colorIndexStr] = 1
                 }
                 else {
-                    ++commitCounts[iy][colorIndexStr];
+                    ++commitCounts[iy][colorIndexStr]
                 }
 
                 //
                 if(commitCounts[iy]['total'] > max) {
-                    max = commitCounts[iy]['total'];
+                    max = commitCounts[iy]['total']
                 }
 
                 //
                 if(!firstDay || (d < firstDay)) {
-                    firstDay = d;
+                    firstDay = d
                 }
 
                 if(!lastDay || (d > lastDay)) {
-                    lastDay = d;
+                    lastDay = d
                 }
             }
         })
-
-        openNextRepo()
     }
 
-    //all repos done
-    else {
-        var currentDayToDraw = firstDay;
-        currentDayToDraw.setDate(currentDayToDraw.getDate() - 3);
+    //
+    return {
+        commitCounts,
+        allColors,
+        max,
+        firstDay,
+        lastDay,
+    }
+}
 
-        var lastDayToDraw = lastDay;
-        lastDayToDraw.setDate(lastDayToDraw.getDate() + 3);
+//
+function renderData(data) {
 
-        var sqHtml = '<div class="sqs">';
-        var divOpen = false;
+    //
+    const currentDayToDraw = data.firstDay
+    currentDayToDraw.setDate(currentDayToDraw.getDate() - 3)
 
-        //
-        while(currentDayToDraw <= lastDayToDraw) {
-            if(!divOpen) {
-                sqHtml += '<div>';
-                divOpen = true;
+    const lastDayToDraw = data.lastDay
+    lastDayToDraw.setDate(lastDayToDraw.getDate() + 3)
 
-                //
-                weeksDays = [];
-                weeksHtml = '';
-            }
+    let sqHtml = '<div class="sqs">'
+    let divOpen = false
+    let weeksDays, weeksHtml
+
+    //
+    while(currentDayToDraw <= lastDayToDraw) {
+        if(!divOpen) {
+            sqHtml += '<div>'
+            divOpen = true
 
             //
-            weeksDays.push(currentDayToDraw.getDate());
+            weeksDays = []
+            weeksHtml = ''
+        }
 
-            var iy = getYyyyMmDd(currentDayToDraw);
-            var numCommits = commitCounts[iy]
-                ? commitCounts[iy]['total']
-                : 0;
+        //
+        weeksDays.push(currentDayToDraw.getDate())
 
-            var percent = numCommits/max;
-            var colorMax = 0;
-            var colorI = 0;
+        const iy = getYyyyMmDd(currentDayToDraw)
+        const numCommits = data.commitCounts[iy]
+            ? data.commitCounts[iy]['total']
+            : 0
 
-            if(commitCounts[iy]) {
+        const percent = numCommits/data.max
+        let colorMax = 0
+        let colorI = 0
 
-                //get the biggest color count
-                for(var i in commitCounts[iy]) {
-                    if(i != 'total') {
-                        if(commitCounts[iy][i] > colorMax) {
-                            colorMax = commitCounts[iy][i];
-                        }
-                    }
-                }
+        if(data.commitCounts[iy]) {
 
-                //select the color with the max count
-                //that has the lowest id
-                colorI = 999999;
-
-                for(var i in commitCounts[iy]) {
-                    if(i != 'total') {
-                        var intI = parseInt(i);
-
-                        if(commitCounts[iy][i] == colorMax && intI < colorI) {
-                            colorI = intI;
-                        }
+            //get the biggest color count
+            for(const i in data.commitCounts[iy]) {
+                if(i != 'total') {
+                    if(data.commitCounts[iy][i] > colorMax) {
+                        colorMax = data.commitCounts[iy][i]
                     }
                 }
             }
 
-            //
-            var cssClass = 'c' + colorI.toString();
+            //select the color with the max count
+            //that has the lowest id
+            colorI = 999999
+
+            for(const i in data.commitCounts[iy]) {
+                if(i != 'total') {
+                    const intI = parseInt(i)
+
+                    if(data.commitCounts[iy][i] == colorMax && intI < colorI) {
+                        colorI = intI
+                    }
+                }
+            }
+        }
+
+        //
+        let cssClass = 'c' + colorI.toString()
+
+        //
+        if(percent <= 0) {
+            cssClass += '-0'
+        }
+        else if(percent > 0 && percent <= .25) {
+            cssClass += '-1'
+        }
+        else if(percent > .25 && percent <= .5) {
+            cssClass += '-2'
+        }
+        else if(percent > .5 && percent <= .75) {
+            cssClass += '-3'
+        }
+        else if(percent > .75) {
+            cssClass += '-4'
+        }
+
+        //
+        weeksHtml += '<div class="' + cssClass + '"></div>'
+
+        if(currentDayToDraw.getDay() == 6) {
 
             //
-            if(percent <= 0) {
-                cssClass += '-0';
-            }
-            if(percent > 0 && percent <= .25) {
-                cssClass += '-1';
-            }
-            else if(percent > .25 && percent <= .5) {
-                cssClass += '-2';
-            }
-            else if(percent > .5 && percent <= .75) {
-                cssClass += '-3';
-            }
-            else if(percent > .75) {
-                cssClass += '-4';
-            }
+            if(weeksDays.indexOf(8) != -1) {
+                const monthLabel = (weeksDays.length == 7)
+                    ? '<label class="month">' +
+                        monthLabels[currentDayToDraw.getMonth()] +
+                        '</label>'
 
-            //
-            weeksHtml += '<div class="' + cssClass + '"></div>';
-
-            if(currentDayToDraw.getDay() == 6) {
+                    : ''
 
                 //
-                if(weeksDays.indexOf(8) != -1) {
-                    var monthLabel = (weeksDays.length == 7)
-                        ? '<label class="month">' +
-                            monthLabels[currentDayToDraw.getMonth()] +
-                            '</label>'
+                const yearLabel = (currentDayToDraw.getMonth() == 6)
+                    ? '<label class="year">' + currentDayToDraw.getFullYear() + '</label>'
+                    : ''
 
-                        : '';
-
-                    //
-                    var yearLabel = (currentDayToDraw.getMonth() == 6)
-                        ? '<label class="year">' + currentDayToDraw.getFullYear() + '</label>'
-                        : '';
-
-                    weeksHtml = yearLabel + monthLabel + weeksHtml;
-                }
-
-                sqHtml += weeksHtml + '</div>';
-                divOpen = false;
+                weeksHtml = yearLabel + monthLabel + weeksHtml
             }
 
-            //
-            currentDayToDraw.setDate(currentDayToDraw.getDate() + 1);
+            sqHtml += weeksHtml + '</div>'
+            divOpen = false
         }
 
         //
-        if(divOpen) {
-            sqHtml += weeksHtml + '</div>';
-        }
-
-        sqHtml += '<span id="bookend"></span>';
-        sqHtml += '</div>';
-
-        //
-        var css = `<style>
-            body {
-                margin-left: 120px;
-                margin-top: 240px;
-            }
-
-            .sqs {
-                position: relative;
-                white-space: nowrap;
-                margin-top: 52px;
-            }
-
-            .sqs > div {
-                display: inline-block;
-                position: relative;
-            }
-
-            .sqs > div:last-of-type {
-                position: absolute;
-                top: 0;
-            }
-
-            .sqs > div > label.month {
-                font-size: 12px;
-                color: #acacac;
-                position: absolute;
-                top: -15px;
-                margin-left: 1px;
-            }
-
-            .sqs > div > label.year {
-                font-size: 16px;
-                color: #777777;
-                position: absolute;
-                top: -42px;
-                left: -50%;
-            }
-
-            .sqs > div > div {
-                width: 10px;
-                height: 10px;
-                margin: 0 0 1px 1px;
-            }
-
-            #bookend {
-                display: inline-block;
-                width: 120px;
-                height: 10px;
-            } `;
-
-        //
-        for(var i in allColors) {
-            var jColors = allColors[i];
-            var sColors = jColors.split(',');
-
-            if(sColors.length == 5) {
-                for(var j in sColors) {
-                    css += '.c' + i + '-' + j +
-                        ' { background: #' +
-                        sColors[j] +
-                        ';} ';
-                }
-            }
-            else {
-                //['eeeeee', 'D6E685', '8CC665', '44A340', '1E6823']
-                var controlColor = (sColors.length > 0)
-                    ? sColors[0]
-                    : '1E6823';
-
-                for(var j = 0; j < 5; ++j) {
-
-                    var attrs = (j == 0)
-                        ? 'background: #eeeeee;'
-                        : 'background: #' + controlColor + '; opacity: ' + (.12 + .22*j) + ';';
-
-                    css += '.c' + i + '-' + j +
-                        ' { ' +
-                        attrs +
-                        ' } ';
-                }
-            }
-        }
-
-        //
-        css += '</style>';
-
-        //
-        fs.writeFile(
-            'index.html',
-            `<!doctype html>
-            <html>
-                <head>${css}</head>
-                <body>
-                    ${sqHtml}
-                </body>
-            </html>`,
-            err => {});
+        currentDayToDraw.setDate(currentDayToDraw.getDate() + 1)
     }
+
+    //
+    if(divOpen) {
+        sqHtml += weeksHtml + '</div>'
+    }
+
+    sqHtml += '<span id="bookend"></span>'
+    sqHtml += '</div>'
+
+    //
+    let css = `<style>
+        body {
+            margin-left: 120px;
+            margin-top: 240px;
+        }
+
+        .sqs {
+            position: relative;
+            white-space: nowrap;
+            margin-top: 52px;
+        }
+
+        .sqs > div {
+            display: inline-block;
+            position: relative;
+        }
+
+        .sqs > div:last-of-type {
+            position: absolute;
+            top: 0;
+        }
+
+        .sqs > div > label.month {
+            font-size: 12px;
+            color: #acacac;
+            position: absolute;
+            top: -15px;
+            margin-left: 1px;
+        }
+
+        .sqs > div > label.year {
+            font-size: 16px;
+            color: #777777;
+            position: absolute;
+            top: -42px;
+            left: -50%;
+        }
+
+        .sqs > div > div {
+            width: 10px;
+            height: 10px;
+            margin: 0 0 1px 1px;
+        }
+
+        #bookend {
+            display: inline-block;
+            width: 120px;
+            height: 10px;
+        } `
+
+    //
+    for(const i in data.allColors) {
+        const jColors = data.allColors[i]
+        const sColors = jColors.split(',')
+
+        if(sColors.length == 5) {
+            for(const j in sColors) {
+                css += '.c' + i + '-' + j +
+                    ' { background: #' +
+                    sColors[j] +
+                    ';} '
+            }
+        }
+        else {
+            const controlColor = (sColors.length > 0)
+                ? sColors[0]
+                : '1E6823'
+
+            for(let j = 0; j < 5; ++j) {
+
+                const attrs = (j == 0)
+                    ? 'background: #eeeeee;'
+                    : 'background: #' + controlColor + '; opacity: ' + (.12 + .22*j) + ';'
+
+                css += '.c' + i + '-' + j +
+                    ' { ' +
+                    attrs +
+                    ' } '
+            }
+        }
+    }
+
+    //
+    css += '</style>'
+
+    //
+    fs.writeFile(
+        'index.html',
+        `<!doctype html>
+        <html>
+            <head>${css}</head>
+            <body>
+                ${sqHtml}
+            </body>
+        </html>`,
+        err => {})
 }
