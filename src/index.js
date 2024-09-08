37c77dda790e444212419e1e286094815489e197
Ry Ferguson <rytf12@gmail.com>
2024-09-08 21:58:22 +0100

    change json config file name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { getData } = require('./lib/get-data.js')
 const { renderData } = require('./lib/render-data.js')
-const config = require('./my-settings')
+const config = require('./config')
 
 getData(config).then(data => {
     renderData(data)
